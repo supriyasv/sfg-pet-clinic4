45aa55bc273382d7479cbf315ec3d8cfdd1fad8e
supriya <supriya.sv@profinch.com>
2020-02-27 10:31:10 +0530

    initial Project Add

---
+++ src/main/java/sup/SpringFramwork/sfgpetclinic4/SfgPetClinic4Application.java
@@ -0,0 +1,13 @@
+package sup.SpringFramwork.sfgpetclinic4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgPetClinic4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgPetClinic4Application.class, args);
+	}
+
+}
---
+++ src/test/java/sup/SpringFramwork/sfgpetclinic4/SfgPetClinic4ApplicationTests.java
@@ -0,0 +1,13 @@
+package sup.SpringFramwork.sfgpetclinic4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinic4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
