0c990e324e45c7d1d9cfa856d4531946589b5170
supriya <supriya.sv@profinch.com>
2020-02-28 16:02:24 +0530

    adding POJOs. Closes #2.

---
+++ src/main/java/sup/SpringFramwork/sfgpetclinic4/model/PetType.java
@@ -0,0 +1,14 @@
+package sup.SpringFramwork.sfgpetclinic4.model;
+
+public class PetType {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/sup/SpringFramwork/sfgpetclinic4/model/Vet.java
@@ -0,0 +1,4 @@
+package sup.SpringFramwork.sfgpetclinic4.model;
+
+public class Vet extends Person {
+}
